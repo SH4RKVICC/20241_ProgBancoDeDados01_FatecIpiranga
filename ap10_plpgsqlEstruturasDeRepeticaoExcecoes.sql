956bd5ed62f9d1df4f837ae3f1b5480b6354a9ab
Victória Amaral <ttoiav.aamaral@gmail.com>
2024-05-12 23:37:30 -0300

    ap10-estruturaderepetiçãoexceções: LOOP c/teste de continuidade EXIT/WHEN

--- ap10_plpgsqlEstruturasDeRepeticaoExcecoes.sql
+++ ap10_plpgsqlEstruturasDeRepeticaoExcecoes.sql
@@ -41,3 +41,21 @@
 --    END LOOP;
 --END;
 --$$
+
+---LOOP: Teste de Continuidade com EXIT/WHEN;
+--A condição será expressa combinando EXIT/WHEN
+
+--Contando de 1 a 10
+--Saída com EXIT/WHEN
+--DO
+--$$
+--DECLARE
+--    contador INT := 1;
+--BEGIN
+--    LOOP
+--        RAISE NOTICE '%', contador;
+--        contador := contador + 1;
+--        EXIT WHEN contador > 10;
+--        END LOOP;
+--END;
+--$$
