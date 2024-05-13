ae87eb40bcdeb099462b3d4f4232dfc9c86f1e3c
Victória Amaral <ttoiav.aamaral@gmail.com>
2024-05-12 23:36:01 -0300

    ap10-estruturaderepetiçãoexceções: Exemplo LOOP

--- ap10_plpgsqlEstruturasDeRepeticaoExcecoes.sql
+++ ap10_plpgsqlEstruturasDeRepeticaoExcecoes.sql
@@ -9,3 +9,15 @@
 
 ---Testando função;
 --SELECT valor_aleatorio_entre(2,10); ---Função realizada com sucesso!
+
+---Exemplo de LOOP;
+--Observe como NÃO é condição de continuidade
+-- Estamos diante de um LOOP infinito
+--DO
+--$$
+--BEGIN
+--    LOOP
+--        RAISE NOTICE 'Teste LOOP simples...';
+--    END LOOP;
+--END;
+--$$ --Eu meio que fiquei esperando a operação ser finalizada, haha. Me esqueci que era infinito.
