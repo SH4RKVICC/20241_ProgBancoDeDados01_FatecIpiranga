28af289ba0754b5f1b91ddae29f2670e037964cc
Victória Amaral <ttoiav.aamaral@gmail.com>
2024-05-12 23:38:14 -0300

    ap10-estruturaderepetiçãoexceções: LOOP c/iteração c/CONTINUE

--- ap10_plpgsqlEstruturasDeRepeticaoExcecoes.sql
+++ ap10_plpgsqlEstruturasDeRepeticaoExcecoes.sql
@@ -59,3 +59,25 @@
 --        END LOOP;
 --END;
 --$$
+
+---LOOP: Ignorando iterações com CONTINUE;
+--Ignorando iterações especificadas
+
+--DO
+--$$
+--DECLARE
+--	contador INT := 0;
+--BEGIN
+--    LOOP
+--		contador := contador + 1;
+--		EXIT WHEN contador > 100;
+--		-- Ignorando iteração da vez quando contador for múltiplo de 7 c/IF/CONTINUE
+--		IF contador % 7 = 0 THEN
+--			CONTINUE;
+--		END IF;
+--		--ignorando iteração da vez quando contador for múltiplo de 11 c/CONTINUE WHEN
+--    	CONTINUE WHEN contador % 11 = 0;
+--		RAISE NOTICE '%', contador;
+--	END LOOP;
+--END;
+--$$
