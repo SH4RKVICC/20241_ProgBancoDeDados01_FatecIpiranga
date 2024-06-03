46d8ad6583d06fd62b8647c02fa70028d078d57e
Victória Amaral <ttoiav.aamaral@gmail.com>
2024-06-03 19:20:55 -0300

    ap09(estrutura-de-selecao): testando função

--- Apostila09_OperadoresLogicosRelacionaisEstruturasDeSelecao/CriandoFuncao.sql
+++ Apostila09_OperadoresLogicosRelacionaisEstruturasDeSelecao/CriandoFuncao.sql
@@ -1,3 +1,4 @@
+-- Criando Função;
 CREATE OR REPLACE FUNCTION valor_aleatorio_entre(lim_inferior INT, lim_superior INT)
 RETURNS INT AS
 $$
@@ -5,3 +6,6 @@ BEGIN
 	RETURN FLOOR(RANDOM()*(lim_superior - lim_inferior)+ lim_inferior)::INT;
 END;
 $$ LANGUAGE plpgsql;
+
+-- Testanto função;
+SELECT valor_aleatorio_entre(2,10);
