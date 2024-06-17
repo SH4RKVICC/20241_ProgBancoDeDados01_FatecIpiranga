31f787b8192ec2f59cde6d3ca447a9d62b4dd75d
Victória Amaral <ttoiav.aamaral@gmail.com>
2024-06-17 17:07:59 -0300

    ap10(exercicio 01): Realizando exerc. 01 utilizando FOR.

---
+++ Apostila10_Exercicios/Exerc01Foreach.sql
@@ -0,0 +1,13 @@
+--- Faça um programa que mostre os números pares entre 1 e 100, inclusive.
+
+DO $$
+DECLARE
+    TYPE num_par IS RECORD (
+        num INTEGER
+    );
+    v_cursor FOR
+    SELECT 
+BEGIN
+
+END;
+$$
