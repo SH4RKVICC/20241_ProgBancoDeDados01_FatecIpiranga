b141eddf244a9b49bdc64457b5dcd76b17e63ac6
Victória Amaral <ttoiav.aamaral@gmail.com>
2024-06-17 21:02:33 -0300

    ap10(exercicio-01): Realizando exerc. 01 utilizando FOREACH.

--- Apostila10_Exercicios/Exerc01Foreach.sql
+++ Apostila10_Exercicios/Exerc01Foreach.sql
@@ -1,13 +1,25 @@
 --- Faça um programa que mostre os números pares entre 1 e 100, inclusive.
 
+-- Faça um programa que mostre os números pares entre 1 e 100, inclusive.
+
 DO $$
 DECLARE
-    TYPE num_par IS RECORD (
-        num INTEGER
-    );
-    v_cursor FOR
-    SELECT 
+    a INT[] := '{}';
+    i INT := 1;
+	vp INT;
+	contador_positivos INT := 0;
 BEGIN
+    WHILE i <= 100 LOOP
+        a := a || ARRAY[i];
+        i := i + 1;
+    END LOOP;
 
+	FOREACH vp IN ARRAY a LOOP
+		IF vp % 2 = 0 THEN
+			RAISE NOTICE '%', vp;
+		END IF;
+	END LOOP;
 END;
 $$
+
+
